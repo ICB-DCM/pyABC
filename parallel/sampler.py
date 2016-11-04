733ea6ebc64358fada1cbc70f20a9e8ecf2796c9
neuralyzer <emmanuel.klinger@gmail.com>
2016-11-04 13:01:11 +0100

    cosmetics

--- parallel/sampler.py
+++ parallel/sampler.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 import random
 
+
 class Sampler(ABC):
     @abstractmethod
     def sample_until_n_accepted(self, sample_one, simulate_one, accept_one, n):
