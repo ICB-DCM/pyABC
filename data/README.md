b598e7393351dcd633b6080cad20700d3d0d2246
neuralyzer <emmanuel.klinger@gmail.com>
2016-11-18 15:33:31 +0100

    add snakemake file

---
+++ data/README.md
@@ -0,0 +1,8 @@
+Workflow
+========
+
+
+1. Scripts in the **producer** folder generate data and store it in the
+2. **raw** folder
+3. Scripts in the **transformer** folder generate and store 
+4. figures in the **figures** folder
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(install_requires=['numpy', 'scipy', 'pandas', 'cloudpickle',
                 'gitpython', 'seaborn', 'scikit-learn',
                 'matplotlib', 'sqlalchemy'],
-     packages=find_packages(exclude=["IPythonNotebooks*", "figures*", "misc*", "pydokuwiki*", "scripts*", "style*"]),
+     packages=find_packages(exclude=["IPythonNotebooks*", "figures*", "misc*", "pydokuwiki*", "scripts*", "style*", "data*"]),
      author='Emmanuel Klinger, Dennis Rickert',
      author_email='emmanuel.klinger@brain.mpg.de, dennis.rickert@helmholtz-muenchen.de',
      name="ICBayes",
