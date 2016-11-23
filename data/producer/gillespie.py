fa6a5c769de8c0defed669047fbfdf94dd7ff9c0
neuralyzer <emmanuel.klinger@gmail.com>
2016-11-23 10:51:03 +0100

    gillespie pre predator implementation

---
+++ data/producer/gillespie.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 15:04:51 2016
+
+@author: emmanuel
+"""
+import scipy as sp
+import matplotlib.pyplot as plt
+import style
+import seaborn as sns
+import time
+
+
+def h(x, pre, c):
+    return (x**pre).prod(1) * c    
+
+
+def gillespie(x, c, pre, post, max_t):
+    """
+    Parameters
+    ----------
+    
+    x: 1D array, size nr_species
+        initial numbers
+    
+    c: 1D array, size nr_reactions
+        reaction rates
+    
+    pre: array nr_reactions x nr_species
+        consumed    
+    
+    post: array nr_reactions x nr_species
+        what is to be produced
+    
+    max_t: int
+        up to where to simulate
+    
+    """
+    t = 0
+    t_store = [t]
+    x_store = [x.copy()]
+    S = post - pre
+
+    while t < max_t:
+        h_vec = h(x, pre, c)
+        h0 = h_vec.sum()
+        delta_t = sp.random.exponential(1/h0)    
+        reaction = sp.random.choice(len(c), p=h_vec/h0)
+        t = t + delta_t
+        x = x + S[reaction]
+        
+        t_store.append(t)
+        x_store.append(x)
+        
+    return sp.array(t_store), sp.array(x_store)
+        
+    
+if __name__ == "__main__"    :
+    x = sp.array([50, 100])   # molecule numbers
+    c = sp.array([1, .005, .6])  # rate constants
+    pre = sp.array([[1, 0],
+                    [1, 1],
+                    [0, 1]], dtype=int)
+    post = sp.array([[2, 0],
+                     [0, 2],
+                     [0, 0]])
+    max_t = 25    
+        
+    start = time.time()    
+    t_store, x_store = gillespie(x, c, pre, post, max_t)    
+    duration = time.time() - start    
+    
+    print(len(t_store), "steps", duration, "duration")
+    
+    fig, ax = plt.subplots()    
+    ax.plot(sp.array(t_store), sp.asarray(x_store))
+    ax.legend(["Prey", "Predator"], loc="center left", bbox_to_anchor=[1, .5])
+    sns.despine(ax=ax)
+    style.middle_ticks_minor(ax)
---
+++ data/producer/stumpf2010preypredator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 10:08:49 2016
+
+@author: emmanuel
+"""
+
+import scipy as sp
+from data.producer.gillespie import gillespie
+import style
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class model1:
+    name = "Model 1"
+    x0 = sp.array([40, 3])   # molecule numbers
+    c = sp.array([2.1])  # rate constants
+    pre = sp.array([[1, 1]], dtype=int)
+    post = sp.array([[0, 2]])
+    max_t = .1
+    
+class model2(model1):
+    name = "Model 2"
+    c = sp.array([30])  # rate constants
+    pre = sp.array([[1, 0]], dtype=int)
+    post = sp.array([[0, 1]])
+    
+    
+    
+fig, axes = plt.subplots(2, 5, sharex=True, sharey=True)
+fig.set_size_inches((style.size.m[0]*5, style.size.m[1]*2))
+for model, row  in zip([model1, model2], axes):
+    row[0].set_ylabel(model.name + "\n" + "Species")
+    for ax in row:
+        t, x = gillespie(model.x0, model.c, model.pre, model.post, model.max_t)
+        ax.plot(t, x)
+        ax.set_xlim(0, model.max_t)
+        ax.set_xticks([0, model.max_t])
+        max_y = 45
+        ax.set_ylim(0, max_y)
+        ax.set_yticks([0, max_y])
+
+for ax in row:
+    ax.set_xlabel(style.name.t, labelpad=style.labelpad.reduced)
+        
+ax.legend(["X", "Y"], loc="center left", bbox_to_anchor=(1, .5))
+sns.despine(fig)
+fig.tight_layout()
+fig.show()
