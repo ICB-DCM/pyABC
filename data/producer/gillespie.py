1dd903ebcb301d322344daa5a3f24b198f0fa170
neuralyzer <emmanuel.klinger@gmail.com>
2016-11-23 11:54:08 +0100

    rare case fixes

--- data/producer/gillespie.py
+++ data/producer/gillespie.py
@@ -11,11 +11,25 @@ import style
 import seaborn as sns
 import time
 
-
 def h(x, pre, c):
-    return (x**pre).prod(1) * c    
+    return (x**pre).prod(1) * c
 
 
+def fast_random_choice(weights):
+    """
+    this is at least for small arrays much faster
+    than numpy.random.choice.
+    For the Gillespie overall this brings for 3 reaction a speedup of a factor of 2
+    """
+    cs = 0
+    u = sp.random.rand()
+    for k in range(weights.size):
+        cs += weights[k]
+        if u <= cs:
+            return k
+    raise Exception("Random choice error {}".format(weights))
+
+    
 def gillespie(x, c, pre, post, max_t):
     """
     Parameters
@@ -45,15 +59,18 @@ def gillespie(x, c, pre, post, max_t):
     while t < max_t:
         h_vec = h(x, pre, c)
         h0 = h_vec.sum()
-        delta_t = sp.random.exponential(1/h0)    
-        reaction = sp.random.choice(len(c), p=h_vec/h0)
+        delta_t = sp.random.exponential(1 / h0)
+        if not sp.isfinite(delta_t):
+            break
+        reaction = fast_random_choice_cython(h_vec/h0)
+        #reaction = sp.random.choice(c.size, p=h_vec/h0)
         t = t + delta_t
         x = x + S[reaction]
         
         t_store.append(t)
         x_store.append(x)
-        
-    return sp.array(t_store), sp.array(x_store)
+
+    return sp.asarray(t_store), sp.asarray(x_store)
         
     
 if __name__ == "__main__"    :
