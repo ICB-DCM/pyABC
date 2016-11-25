d3a5466ebf97e38628af86592e79958a7bb6ad74
neuralyzer <emmanuel.klinger@gmail.com>
2016-11-25 14:44:30 +0100

    add numpy to bytes storage helper functions

---
+++ pyabc/test/test_numpy_bytes_storage.py
@@ -0,0 +1,14 @@
+import pytest
+import numpy as np
+from pyabc.storage import np_from_bytes, np_to_bytes
+
+
+@pytest.fixture
+def rand_arr():
+    arr = np.random.rand(3, 8)
+    return arr
+
+
+def test_storage(rand_arr):
+    arr = np_from_bytes(np_to_bytes(rand_arr))
+    assert (arr == rand_arr).all()
