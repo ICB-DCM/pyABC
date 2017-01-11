24ef4152b5b7eedae286d14f4c49807cdc71d691
neuralyzer <emmanuel.klinger@gmail.com>
2017-01-11 14:22:14 +0100

    fix import

--- parallel/__init__.py
+++ parallel/__init__.py
@@ -3,7 +3,7 @@ Parallel job execution
 ======================
 """
 
-from parallel.util import nr_cores_available
+from .util import nr_cores_available
 from .execution_contexts import DefaultContext, ProfilingContext
 from .sge import SGE
 from .util import sge_available
