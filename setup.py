892d8828dfc99cecbd7fe9cc7b3588d9f3252d2e
neuralyzer <emmanuel.klinger@gmail.com>
2017-01-11 09:54:13 +0100

    add missing dependencies

--- setup.py
+++ setup.py
@@ -12,21 +12,27 @@ setup(install_requires=['numpy', 'scipy', 'pandas', 'cloudpickle',
                 "flask", "flask_bootstrap", "bokeh", "redis",
                 "dill",
                 'gitpython', 'seaborn', 'scikit-learn',
-                'matplotlib', 'sqlalchemy'],
-     packages=["pyabc", "parallel"],
-     author='Emmanuel Klinger, Dennis Rickert',
-     author_email='emmanuel.klinger@brain.mpg.de, dennis.rickert@helmholtz-muenchen.de',
-     name="pyabc",
-     version="0.1.1",
-     license="GPLv3",
-     platforms="all",
-     url="https://github.com/neuralyzer/pyabc",
-     include_package_data=True,
-     description='Massively parallel ABC for Python',
-      zip_safe=False,  # not zip safe b/c of Flask templates
-      entry_points={
-            'console_scripts': [
-                  'abc-server = visserver.server:run_app',
-            ]
-      },
+                'matplotlib', 'sqlalchemy',
+                'distributed'],
+    packages=["pyabc", "parallel", "visserver"],
+    author='Emmanuel Klinger, Dennis Rickert',
+    author_email='emmanuel.klinger@brain.mpg.de, dennis.rickert@helmholtz-muenchen.de',
+    name="pyabc",
+    version="0.1.3",
+    license="GPLv3",
+    platforms="all",
+    url="https://github.com/neuralyzer/pyabc",
+    include_package_data=True,
+    description='Massively parallel ABC for Python',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+   keywords='inference abc approximate bayesian computation parallel distributed',
+    zip_safe=False,  # not zip safe b/c of Flask templates
+    entry_points={
+        'console_scripts': [
+              'abc-server = visserver.server:run_app',
+        ]
+},
       )
