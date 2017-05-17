d07a16aaf4dd863d665b27b58f629d99f59fb89f
neuralyzer <emmanuel.klinger@gmail.com>
2017-05-17 11:02:16 +0200

    sge doc update

--- pyabc/sge/__init__.py
+++ pyabc/sge/__init__.py
@@ -1,6 +1,15 @@
 """
 Parallel job execution on SGE like environments
 ===============================================
+
+The functions and classes in the pyabc.sge package can be used for at least
+two purposes:
+
+1. The SGE.map method can be used together with the MappingSampler to
+   parallelize ABC-SMC in a SGE/UGE infrastructure.
+2. SGE.map can be used in a standalone mode to execute jobs on a SGE/UGE
+   cluster. This is completely independent of ABC-SMC inference.
+
 """
 
 from .util import nr_cores_available
