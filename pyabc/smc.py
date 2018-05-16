346ba3949103e70658ac015fe2992a4ea36dd4f5
yannikschaelte <yannik.schaelte@gmail.com>
2018-05-16 08:40:54 +0200

    assure passed acceptor to smc is acceptor

--- pyabc/smc.py
+++ pyabc/smc.py
@@ -25,7 +25,7 @@ from typing import Union
 from .model import SimpleModel
 from .populationstrategy import ConstantPopulationSize
 from .platform_factory import DefaultSampler
-from .acceptor import accept_use_current_time
+from .acceptor import accept_use_current_time, SimpleAcceptor
 import copy
 import warnings
 
@@ -203,7 +203,7 @@ class ABCSMC:
 
         if acceptor is None:
             acceptor = accept_use_current_time
-        self.acceptor = acceptor
+        self.acceptor = SimpleAcceptor.assert_acceptor(acceptor)
 
         self.stop_if_only_single_model_alive = False
         self.x_0 = None
