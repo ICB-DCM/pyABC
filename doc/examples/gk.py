45cfc76ac631fa9a0f52dcc31c6007c9e6c5bd68
yannikschaelte <yannik.schaelte@gmail.com>
2018-05-18 07:03:37 +0200

    gk function implemented. how to do analysis not clear yet

--- doc/examples/gk.py
+++ doc/examples/gk.py
@@ -1,5 +1,36 @@
-import scipy as sp
-import numpy as np
+import scipy
+import scipy.stats
+import numpy
 
-def gk():
-    sp.rand()
+
+c = 0.8
+
+
+def gk(pars):
+    # extract parameters
+    A = pars['A']
+    B = pars['B']
+    g = pars['g']
+    k = pars['k']
+
+    # sample from normal distribution
+    z = numpy.random.normal(0, 1)
+
+    # to sample from gk distribution
+    e = numpy.exp(-g*z)
+    return A + B * (1 + c * (1-e) / (1+e)) * (1 + z**2)**k * z
+
+
+theta0 = {'A': 3.0,
+          'B': 1.0,
+          'g': 1.5,
+          'k': 0.5}
+
+
+# observed sum stats
+
+# prior
+
+# distance
+
+# abc
