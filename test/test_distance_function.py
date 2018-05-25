0607f0b4c66b338dc68a5821eaa0f6f98932c51f
yannikschaelte <yannik.schaelte@gmail.com>
2018-05-25 22:40:58 +0200

    fix test initialize(x_0)

--- test/test_distance_function.py
+++ test/test_distance_function.py
@@ -15,7 +15,7 @@ class MockABC:
 def test_single_parameter():
     dist_f = MinMaxDistanceFunction(measures_to_use=["a"])
     abc = MockABC([{"a": -3}, {"a": 3}, {"a": 10}])
-    dist_f.initialize(0, abc.sample_from_prior())
+    dist_f.initialize(0, abc.sample_from_prior(), {})
     d = dist_f(0, {"a": 1}, {"a": 2})
     assert 1/13 == d
 
@@ -23,7 +23,7 @@ def test_single_parameter():
 def test_two_parameters_but_only_one_used():
     dist_f = MinMaxDistanceFunction(measures_to_use=["a"])
     abc = MockABC([{"a": -3, "b": 2}, {"a": 3, "b": 3}, {"a": 10, "b": 4}])
-    dist_f.initialize(0, abc.sample_from_prior())
+    dist_f.initialize(0, abc.sample_from_prior(), {})
     d = dist_f(0, {"a": 1, "b": 10}, {"a": 2, "b": 12})
     assert 1/13 == d
 
@@ -31,7 +31,7 @@ def test_two_parameters_but_only_one_used():
 def test_two_parameters_and_two_used():
     dist_f = MinMaxDistanceFunction(measures_to_use=["a", "b"])
     abc = MockABC([{"a": -3, "b": 2}, {"a": 3, "b": 3}, {"a": 10, "b": 4}])
-    dist_f.initialize(0, abc.sample_from_prior())
+    dist_f.initialize(0, abc.sample_from_prior(), {})
     d = dist_f(0, {"a": 1, "b": 10}, {"a": 2, "b": 12})
     assert 1/13 + 2/2 == d
 
@@ -39,7 +39,7 @@ def test_two_parameters_and_two_used():
 def test_single_parameter_percentile():
     dist_f = PercentileDistanceFunction(measures_to_use=["a"])
     abc = MockABC([{"a": -3}, {"a": 3}, {"a": 10}])
-    dist_f.initialize(0, abc.sample_from_prior())
+    dist_f.initialize(0, abc.sample_from_prior(), {})
     d = dist_f(0, {"a": 1}, {"a": 2})
     expected = (
         1 / (sp.percentile([-3, 3, 10], 80) - sp.percentile([-3, 3, 10], 20))
@@ -53,7 +53,7 @@ def test_pnormdistance():
 
     # first test that for PNormDistance, the weights stay constant
     dist_f = PNormDistance(p=2)
-    dist_f.initialize(0, abc.sample_from_prior())
+    dist_f.initialize(0, abc.sample_from_prior(), {})
 
     # call distance function, also to initialize w
     d = dist_f(0, abc.sample_from_prior()[0], abc.sample_from_prior()[1])
