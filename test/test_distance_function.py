be11c067573132a38d9c78ba34b8e5eb69237a35
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-17 04:33:38 +0100

    add basic test for adaptivepnormdistance

--- test/test_distance_function.py
+++ test/test_distance_function.py
@@ -1,7 +1,21 @@
 import scipy as sp
 from pyabc import (PercentileDistanceFunction,
                    MinMaxDistanceFunction,
-                   PNormDistance)
+                   PNormDistance,
+                   AdaptivePNormDistance)
+
+
+from pyabc.distance_functions import (
+    median_absolute_deviation,
+    mean_absolute_deviation,
+    standard_deviation,
+    bias,
+    root_mean_square_deviation,
+    median_absolute_deviation_to_observation,
+    mean_absolute_deviation_to_observation,
+    combined_median_absolute_deviation,
+    combined_mean_absolute_deviation,
+    standard_deviation_to_observation)
 
 
 class MockABC:
@@ -15,7 +29,7 @@ class MockABC:
 def test_single_parameter():
     dist_f = MinMaxDistanceFunction(measures_to_use=["a"])
     abc = MockABC([{"a": -3}, {"a": 3}, {"a": 10}])
-    dist_f.initialize(0, abc.sample_from_prior(), {})
+    dist_f.initialize(0, abc.sample_from_prior())
     d = dist_f(0, {"a": 1}, {"a": 2})
     assert 1/13 == d
 
@@ -23,7 +37,7 @@ def test_single_parameter():
 def test_two_parameters_but_only_one_used():
     dist_f = MinMaxDistanceFunction(measures_to_use=["a"])
     abc = MockABC([{"a": -3, "b": 2}, {"a": 3, "b": 3}, {"a": 10, "b": 4}])
-    dist_f.initialize(0, abc.sample_from_prior(), {})
+    dist_f.initialize(0, abc.sample_from_prior())
     d = dist_f(0, {"a": 1, "b": 10}, {"a": 2, "b": 12})
     assert 1/13 == d
 
@@ -31,7 +45,7 @@ def test_two_parameters_but_only_one_used():
 def test_two_parameters_and_two_used():
     dist_f = MinMaxDistanceFunction(measures_to_use=["a", "b"])
     abc = MockABC([{"a": -3, "b": 2}, {"a": 3, "b": 3}, {"a": 10, "b": 4}])
-    dist_f.initialize(0, abc.sample_from_prior(), {})
+    dist_f.initialize(0, abc.sample_from_prior())
     d = dist_f(0, {"a": 1, "b": 10}, {"a": 2, "b": 12})
     assert 1/13 + 2/2 == d
 
@@ -39,7 +53,7 @@ def test_two_parameters_and_two_used():
 def test_single_parameter_percentile():
     dist_f = PercentileDistanceFunction(measures_to_use=["a"])
     abc = MockABC([{"a": -3}, {"a": 3}, {"a": 10}])
-    dist_f.initialize(0, abc.sample_from_prior(), {})
+    dist_f.initialize(0, abc.sample_from_prior())
     d = dist_f(0, {"a": 1}, {"a": 2})
     expected = (
         1 / (sp.percentile([-3, 3, 10], 80) - sp.percentile([-3, 3, 10], 20))
@@ -53,7 +67,7 @@ def test_pnormdistance():
 
     # first test that for PNormDistance, the weights stay constant
     dist_f = PNormDistance(p=2)
-    dist_f.initialize(0, abc.sample_from_prior(), {})
+    dist_f.initialize(0, abc.sample_from_prior())
 
     # call distance function, also to initialize w
     d = dist_f(0, abc.sample_from_prior()[0], abc.sample_from_prior()[1])
@@ -63,4 +77,31 @@ def test_pnormdistance():
     assert sum(abs(a-b) for a, b in
                zip(list(dist_f.w[0].values()), [1, 1, 1])) < 0.01
 
-    # TODO: Also create test for AdaptivePNormDistance
+
+def test_adaptivepnormdistance():
+    """
+    So far only tests basic running.
+    """
+    abc = MockABC([{'s1': -1, 's2': -1, 's3': -1},
+                   {'s1': -1, 's2': 0, 's3': 1}])
+    x_0 = {'s1': 0, 's2': 0, 's3': 1}
+
+    scale_functions = [
+        median_absolute_deviation,
+        mean_absolute_deviation,
+        standard_deviation,
+        bias,
+        root_mean_square_deviation,
+        median_absolute_deviation_to_observation,
+        mean_absolute_deviation_to_observation,
+        combined_median_absolute_deviation,
+        combined_mean_absolute_deviation,
+        standard_deviation_to_observation
+    ]
+
+    for scale_function in scale_functions:
+        dist_f = AdaptivePNormDistance(
+            x_0=x_0, p=2, scale_function=scale_function)
+
+        dist_f.initialize(0, abc.sample_from_prior())
+        dist_f(0, abc.sample_from_prior()[0], abc.sample_from_prior()[1])
