21b02cca9d038ef788ad36b74915c5063c4bdc7d
yannikschaelte <yannik.schaelte@gmail.com>
2019-02-01 21:50:07 +0100

    fix flake8

--- test/test_storage.py
+++ test/test_storage.py
@@ -116,7 +116,7 @@ def test_savs_no_sum_stats(history: History):
             accepted=True)
         particle_list.append(particle)
     population = Population(particle_list)
-    
+
     # do not save sum stats
     print(history.stores_sum_stats)
     history.stores_sum_stats = False
