aa8c528336d4ba1c05cedaa718061410621d26c9
yannikschaelte <yannik.schaelte@gmail.com>
2019-03-23 22:02:17 +0100

    viz: add doc for epsilon, sample

--- pyabc/visualization/sample.py
+++ pyabc/visualization/sample.py
@@ -10,9 +10,31 @@ def plot_sample_numbers(
         histories: Union[List, History],
         labels: Union[List, str] = None,
         rotation: int = 0,
+        title: str = "Total required samples",
         size: tuple = None):
     """
     Plot required numbers of samples over all iterations.
+
+    Parameters
+    ----------
+
+    histories: Union[List, History]
+        The histories to plot from. History ids must be set correctly.
+    labels: Union[List ,str], optional
+        Labels corresponding to the histories. If None are provided,
+        indices are used as labels.
+    rotation: int, optional (default = 0)
+        Rotation to apply to the plot's x tick labels. For longer labels,
+        a tilting of 45 or even 90 can be preferable.
+    title: str, optional (default = "Total required samples")
+        Title for the plot.
+    size: tuple of float, optional
+        The size of the plot in inches.
+
+    Returns
+    -------
+
+    ax: Axis of the generated plot.
     """
     # preprocess input
     histories, labels = to_lists_or_default(histories, labels)
@@ -25,11 +47,13 @@ def plot_sample_numbers(
     # extract sample numbers
     samples = []
     for history in histories:
+        # note: the first entry corresponds to the calibration and should
+        # be included here to be fair against methods not requiring
+        # calibration
         samples.append(np.array(history.get_all_populations()['samples']))
 
     # create matrix
     n_pop = max(len(sample) for sample in samples)
-
     matrix = np.zeros((n_pop, n_run))
     for i_sample, sample in enumerate(samples):
         matrix[:len(sample), i_sample] = sample
@@ -43,13 +67,12 @@ def plot_sample_numbers(
     # add labels
     ax.set_xticks(np.arange(n_run))
     ax.set_xticklabels(labels, rotation=rotation)
-    ax.set_title("Total required samples")
+    ax.set_title(title)
     ax.set_ylabel("Samples")
     ax.set_xlabel("Run")
     # set size
     if size is not None:
         fig.set_size_inches(size)
-
     fig.tight_layout()
 
     return ax
