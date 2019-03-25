a1b4217bf1746a6915ef974dc872e0a05fba2b7b
yannikschaelte <yannik.schaelte@gmail.com>
2019-03-25 13:32:01 +0100

    add remark on #66 rpy2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
                       "feather-format>=0.4.0", "bkcharts>=0.2",
                       "distributed>=1.23.3", "pygments>=2.2.0",
                       "IPython>=7.0.1"],
-    extra_requires={"R": ["rpy2==2.9.4"]},
+    extra_requires={"R": ["rpy2==2.9.5"]},  # until #66 is fixed
     python_requires='>=3.6',
     packages=find_packages(exclude=["examples*", "devideas*",
                                     "test*", "test"]),
