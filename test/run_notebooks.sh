7c097015f737b8fc58973c902f5bd4b8a388cf90
yannikschaelte <yannik.schaelte@gmail.com>
2019-03-28 14:20:43 +0100

    delete some notebooks from test

--- test/run_notebooks.sh
+++ test/run_notebooks.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
+# Run selected notebooks, on error show output and return with error code.
+
+
 declare -a notebooks
-notebooks=("adaptive_distances" "chemical_reaction" "conversion_reaction" "early_stopping" "multiscale_agent_based" "parameter_inference" "quickstart" "resuming" "using_R")
+notebooks=("adaptive_distances" "conversion_reaction" "early_stopping" "parameter_inference" "quickstart" "resuming" "using_R")
 
 
 dir="doc/examples"
@@ -9,6 +13,7 @@ dir="doc/examples"
 
 run_notebook () {
     tempfile=$(tempfile)
+    echo $@
     jupyter nbconvert --ExecutePreprocessor.timeout=-1 --debug --stdout --execute --to markdown $@ &> $tempfile
     ret=$?
     if [[ $ret != 0 ]]; then
