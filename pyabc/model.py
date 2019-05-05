7efd7779efff9906fe133a39e9b208958a016187
yannikschaelte <yannik.schaelte@gmail.com>
2019-05-05 15:11:26 +0200

    set ModelResult weight default to 1.0

--- pyabc/model.py
+++ pyabc/model.py
@@ -20,7 +20,7 @@ class ModelResult:
     """
 
     def __init__(self,
-                 sum_stats=None, distance=None, accepted=None, weight=None):
+                 sum_stats=None, distance=None, accepted=None, weight=1.0):
         self.sum_stats = sum_stats if sum_stats is not None else {}
         self.distance = distance
         self.accepted = accepted
