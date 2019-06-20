5eb9571a5e015d9ee6a5f34caefb7a402a776ad1
yannikschaelte <yannik.schaelte@gmail.com>
2019-06-20 07:26:42 +0200

    tidy up and move to test the xmlManager

--- test/xmlManager.py
+++ test/xmlManager.py
@@ -1,8 +1,8 @@
 from lxml import etree
 
 
-def xmlGenerator(self,para:dict=None):
-    self.param=para
+def xmlGenerator(self, para: dict = None):
+    self.param = para
     # create the file structure
     root = etree.Element('MorpheusModel')
     root.set('version', '3')
@@ -15,17 +15,16 @@ def xmlGenerator(self,para:dict=None):
     details.text = self.param['Details']
 
     # Fill xml file with Global info
-    globala = etree.SubElement(root, "Global")
+    etree.SubElement(root, "Global")
 
     # Fill xml file with Space info
     space = etree.SubElement(root, "Space")
     lattice = etree.SubElement(space, "Lattice")
-    size = etree.SubElement(lattice, "Size")
+    etree.SubElement(lattice, "Size")
     neighborhood = etree.SubElement(lattice, "Neighborhood")
-    order = etree.SubElement(neighborhood, "Order")
+    etree.SubElement(neighborhood, "Order")
     spaceSymbol = etree.SubElement(space, "SpaceSymbol")
     spaceSymbol.set("symbol", self.param['SpaceSymbolSymbol'])
-
 
     # Fill xml file with Time info
     time = etree.SubElement(root, "Time")
@@ -44,28 +43,27 @@ def xmlGenerator(self,para:dict=None):
     cellType.set("class", self.param['CellTypeClass'])
     cellType.set("name", self.param['CellTypeName'])
 
-    property = etree.SubElement(cellType, "Property")
+    etree.SubElement(cellType, "Property")
     system = etree.SubElement(cellType, "System")
-    constant = etree.SubElement(system, "Constant")
+    etree.SubElement(system, "Constant")
     diffEqn = etree.SubElement(system, "DiffEqn")
-    expression = etree.SubElement(diffEqn, "Expression")
-
+    etree.SubElement(diffEqn, "Expression")
 
     # Fill xml file with CellPopulation info
     cellPopulation = etree.SubElement(root, "CellPopulation")
     population = etree.SubElement(cellPopulation, "Population")
     population.set("size", self.param['PopulationSize'])
     population.set("type", self.param['PopulationType'])
-    populationArgument = etree.SubElement(cellPopulation, "Population")
-
+    etree.SubElement(cellPopulation, "Population")
 
     # Fill xml file with Analysis info
-    Analysis = etree.SubElement(root, "Analysis")
-
-
+    etree.SubElement(root, "Analysis")
 
     # create and write the new XML file with the results
-    mydata = etree.tostring(root)
+    etree.tostring(root)
     tree = etree.ElementTree(root)
-    tree.write("model.xml",pretty_print=True,xml_declaration=True, encoding='UTF-8')
-
+    tree.write(
+        "model.xml",
+        pretty_print=True,
+        xml_declaration=True,
+        encoding='UTF-8')
