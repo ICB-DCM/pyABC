a98164f71a8ab64823b353a958a703d26a602c6f
emad <emad>
2019-08-20 17:40:36 +0200

    * fixing issue #38 * MinMaxDistanceFunction -> MinMaxDistance in the test_abc_smc_algorithm &test_samplerperf * PercentileDistanceFunction -> PercentileDistance in the test_abc_smc_algorithm &test_samplerperf

--- pyabc/visualization/data_plot.py
+++ pyabc/visualization/data_plot.py
@@ -3,46 +3,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_data(time, obs_data, sim_data):
-
+def plot_data(obs_data: dict, sim_data: dict, key=None):
+    fig = plt.figure()
+    plot_index = 1
+    # check if user specify a specific key to be printed
+    if key is not None:
+        obs_data = {key: obs_data[key]}
+        sim_data = {key: sim_data[key]}
+    if len(obs_data) <= 3:
+        plot_row_size = 1
+        plot_col_size = len(obs_data)
+    elif len(obs_data) <= 6:
+        plot_row_size = 2
+        plot_col_size = 2
+    elif len(obs_data) <= 9:
+        plot_row_size = 3
+        plot_col_size = 3
+    elif len(obs_data) <= 16:
+        plot_row_size = 4
+        plot_col_size = 4
     # check if the data types are pandas dataframe
-    if isinstance(obs_data, pd.DataFrame)\
-            and isinstance(sim_data, pd.DataFrame):
-        obs_value = obs_data.values.item()
-        sim_value = sim_data.values.item()
-        plt.scatter(time, sim_value,
-                    color="C0", label='Simulation')
-        plt.scatter(time, obs_value,
-                    color="C1", label='Data')
-        plt.xlabel('Time $t$')
-        plt.ylabel('Measurement $Y$')
-        plt.title('Observed vs. simulated data')
-        plt.legend()
+    for (obs_key, obs), (sim_key, sim) \
+            in zip(obs_data.items(), sim_data.items()):
+        plt.subplot(plot_row_size, plot_col_size, plot_index)
+        plot_index = plot_index + 1
+        if isinstance(obs, pd.DataFrame)\
+                and isinstance(sim, pd.DataFrame):
+            obs_value = obs.values.item()
+            sim_value = sim.values.item()
+            plt.plot(sim_value,
+                     color="C0", label='Simulation')
+            plt.plot(obs_value,
+                     color="C1", label='Data')
         # check if the data types are 2d array
-    elif len(obs_data) == 2 and len(sim_data) == 2 \
-            and isinstance(sim_data, list)\
-            and isinstance(obs_data, list):
-        plt.scatter(sim_data[0], sim_data[1],
-                    color="C0", label='Simulation')
-        plt.scatter(obs_data[0], obs_data[1],
-                    color="C1", label='Data')
+        elif isinstance(obs, np.ndarray) \
+                and isinstance(sim, np.ndarray) \
+                and (obs.ndim == 2) and (sim.ndim == 2):
+            obs_value = obs
+            sim_value = sim
+            plt.scatter(sim_value[0],
+                        sim_value[1], color="C0", label='Simulation')
+            plt.scatter(obs_value[0],
+                        obs_value[1], color="C1", label='Data')
+        # check if the data types are numpy array
+        elif isinstance(obs, np.ndarray) \
+                and isinstance(sim, np.ndarray):
+            obs_value = obs
+            sim_value = sim
+            plt.plot(sim_value,
+                     color="C0", label='Simulation')
+            plt.plot(obs_value,
+                     color="C1", label='Data')
+        else:
+            raise SyntaxError('The selected data type is '
+                              'not yet supported. Try to use '
+                              'Pandas.Dataframe, numpy.array, or 2d arrays')
         plt.xlabel('Time $t$')
         plt.ylabel('Measurement $Y$')
-        plt.title('Observed vs. simulated data')
+        plt.title(obs_key)
         plt.legend()
-    # check if the data types are numpy array
-    elif isinstance(obs_data, np.ndarray) \
-            and isinstance(sim_data, np.ndarray):
-        plt.scatter(time, sim_data,
-                    color="C0", label='Simulation')
-        plt.scatter(time, obs_data,
-                    color="C1", label='Data')
-        plt.xlabel('Time $t$')
-        plt.ylabel('Measurement $Y$')
-        plt.title('Observed vs. simulated data')
-        plt.legend()
-    else:
-        raise SyntaxError('The selected data type is '
-                          'not yet supported. Try to use '
-                          'Pandas.Dataframe, numpy.array, or 2d arrays')
-    plt.show()
+        fig.suptitle("Observed vs. simulated data", fontsize=16)
