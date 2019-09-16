5dcd354ec3245651a32be945273c4daa5116f9e1
EmadAlamoudi <emad-24@hotmail.com>
2019-09-16 14:40:33 +0200

    small fix, to enhance code quality

--- pyabc/visualization/data_plot.py
+++ pyabc/visualization/data_plot.py
@@ -59,9 +59,9 @@ def plot_data(obs_data: dict, sim_data: dict, key=None):
                      color="C1", label='Data')
         else:
             print('The selected data type is '
-                              'not yet supported. Try to use '
-                              'Pandas.Dataframe, 1d numpy.array, '
-                              'or 2d numpy.array')
+                  'not yet supported. Try to use '
+                  'Pandas.Dataframe, 1d numpy.array, '
+                  'or 2d numpy.array')
             return -1
         plt.xlabel('Time $t$')
         plt.ylabel('Measurement $Y$')
