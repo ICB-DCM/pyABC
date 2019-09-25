8da2c2b8006c57e89761ad5187e3bcdfc6527718
yannikschaelte <yannik.schaelte@gmail.com>
2019-09-25 09:05:19 +0200

    add plot_total_required_samples plot with yscale

--- pyabc/visualization/__init__.py
+++ pyabc/visualization/__init__.py
@@ -11,7 +11,8 @@ from .kde import (
     plot_kde_2d,
     plot_kde_matrix)
 from .sample import (
-    plot_sample_numbers)
+    plot_sample_numbers,
+    plot_total_sample_numbers)
 from .epsilon import (
     plot_epsilons)
 from .histogram import (
@@ -37,6 +38,7 @@ __all__ = [
     "plot_kde_2d",
     "plot_kde_matrix",
     "plot_sample_numbers",
+    "plot_total_sample_numbers",
     "plot_epsilons",
     "plot_histogram_1d",
     "plot_histogram_1d_lowlevel",
