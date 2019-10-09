5fb47ca12fba8c14436539999a4f2a89f122d8c1
yannikschaelte <yannik.schaelte@gmail.com>
2019-10-09 05:52:34 +0200

    test storage of near-primitive types; fix non-used types

--- test/test_bytesstorage.py
+++ test/test_bytesstorage.py
@@ -1,5 +1,6 @@
 import pytest
 from pyabc.storage.bytes_storage import to_bytes, from_bytes
+from pyabc.storage.numpy_bytes_storage import _primitive_types
 import pandas as pd
 import numpy as np
 import scipy as sp
@@ -12,13 +13,19 @@ import tempfile
 import os
 
 
-@pytest.fixture(params=["empty", "int", "float", "non_numeric_str",
-                        "numeric_str", "int-float-numeric_str",
-                        "int-float-non_numeric_str-str_ind",
-                        "int-float-numeric_str-str_ind",
+@pytest.fixture(params=["empty",
+                        "df-int", "df-float", "df-non_numeric_str",
+                        "df-numeric_str", "df-int-float-numeric_str",
+                        "df-int-float-non_numeric_str-str_ind",
+                        "df-int-float-numeric_str-str_ind",
                         "py-int",
                         "py-float",
                         "py-str",
+                        "np-int",
+                        "np-float",
+                        "np-str",
+                        "np-single-float",
+                        "np-single-str",
                         "r-df-cars",
                         "r-df-faithful",
                         "r-df-iris",
@@ -27,31 +34,31 @@ def object_(request):
     par = request.param
     if par == "empty":
         return pd.DataFrame()
-    if par == "int":
+    if par == "df-int":
         return pd.DataFrame({"a": sp.random.randint(-20, 20, 100),
                              "b": sp.random.randint(-20, 20, 100)})
-    if par == "float":
+    if par == "df-float":
         return pd.DataFrame({"a": sp.randn(100),
                              "b": sp.randn(100)})
-    if par == "non_numeric_str":
+    if par == "df-non_numeric_str":
         return pd.DataFrame({"a": ["foo", "bar"],
                              "b": ["bar", "foo"]})
 
-    if par == "numeric_str":
+    if par == "df-numeric_str":
         return pd.DataFrame({"a": list(map(str, sp.randn(100))),
                              "b": list(map(str,
                                            sp.random.randint(-20, 20, 100)))})
-    if par == "int-float-numeric_str":
+    if par == "df-int-float-numeric_str":
         return pd.DataFrame({"a": sp.random.randint(-20, 20, 100),
                              "b": sp.randn(100),
                              "c": list(map(str,
                                            sp.random.randint(-20, 20, 100)))})
-    if par == "int-float-non_numeric_str-str_ind":
+    if par == "df-int-float-non_numeric_str-str_ind":
         return pd.DataFrame({"a": [1, 2],
                              "b": [1.1, 2.2],
                              "c": ["foo", "bar"]},
                             index=["first", "second"])
-    if par == "int-float-numeric_str-str_ind":
+    if par == "df-int-float-numeric_str-str_ind":
         return pd.DataFrame({"a": [1, 2],
                              "b": [1.1, 2.2],
                              "c": ["1", "2"]},
@@ -66,6 +73,12 @@ def object_(request):
         return sp.random.randint(-20, 20, 100)
     if par == "np-float":
         return sp.random.randn(100)
+    if par == "np-str":
+        return np.array(["foo", "bar"])
+    if par == "np-single-float":
+        return np.array(4.2)
+    if par == "np-single-str":
+        return np.array("foo bar")
     if par == "r-df-cars":
         return r["mtcars"]
     if par == "r-df-iris":
@@ -78,11 +91,13 @@ def object_(request):
 def test_storage(object_):
     serial = to_bytes(object_)
     assert isinstance(serial, bytes)
+
     rebuilt = from_bytes(serial)
 
-    if not isinstance(object_, robjects.DataFrame):
-        assert isinstance(object_, type(rebuilt))
+    # check type
+    _check_type(object_, rebuilt)
 
+    # check value
     if isinstance(object_, int):
         assert object_ == rebuilt
     elif isinstance(object_, float):
@@ -99,6 +114,25 @@ def test_storage(object_):
                    .all().all()
     else:
         raise Exception("Could not compare")
+
+
+def _check_type(object_, rebuilt):
+    # r objects are transformed to pd.DataFrame
+    if isinstance(object_, robjects.DataFrame):
+        assert isinstance(rebuilt, pd.DataFrame)
+    # <= 1 dim numpy arrays are converted to primitive type
+    elif isinstance(object_, np.ndarray) and object_.size <= 1:
+        for type_ in _primitive_types:
+            try:
+                if type_(object_) == object_:
+                    assert isinstance(rebuilt, type_)
+                    return
+            except (TypeError, ValueError):
+                pass
+        raise Exception("Could not check type")
+    # all others keep their type
+    else:
+        assert isinstance(rebuilt, type(object_))
 
 
 def test_reference_parameter():
