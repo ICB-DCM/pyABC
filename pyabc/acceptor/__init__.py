332ebf5702c2221ec269a35e0c4d0e34662da239
yannikschaelte <yannik.schaelte@gmail.com>
2019-10-17 09:26:06 +0200

    create acceptor folder, introduce UniformAcceptor

---
+++ pyabc/acceptor/__init__.py
@@ -0,0 +1,22 @@
+"""
+Acceptor
+========
+
+Acceptors handle the acceptance step.
+
+"""
+
+
+from .acceptor import (
+    Acceptor,
+    SimpleFunctionAcceptor,
+    UniformAcceptor,
+)
+
+
+__all__ = [
+    # acceptor
+    'Acceptor',
+    'SimpleFunctionAcceptor',
+    'UniformAcceptor',
+]
