86a159982b81d4c8454e81693e5844fa4865a39a
yannikschaelte <yannik.schaelte@gmail.com>
2019-10-20 13:52:07 +0200

    add acceptor test

--- test/test_acceptor.py
+++ test/test_acceptor.py
@@ -1,23 +1,24 @@
-from pyabc import (
-    Distribution, RV,
-    SimpleFunctionAcceptor,
-    NoDistance,
-    ABCSMC
-)
+import numpy as np
+
+import pyabc
 from pyabc.acceptor import AcceptorResult
 
 
 def test_simple_acceptor():
+
+    def distance(x, x_0):
+        return sum(abs(x[key] - x_0[key]) for key in x_0)
+
     def dummy_accept(dist, eps, x, x_0, t, par):
-        d = sum(abs(x[key] - x_0[key]) for key in x_0)
-        return AcceptorResult(d, d < 1)
+        d = dist(x, x_0)
+        return AcceptorResult(d, d < eps(t))
 
     x = {'s0': 1, 's1': 0}
     y = {'s0': 2, 's1': 2}
 
-    acceptor = SimpleFunctionAcceptor(dummy_accept)
+    acceptor = pyabc.SimpleFunctionAcceptor(dummy_accept)
 
-    ret = acceptor(distance_function=None, eps=None,
+    ret = acceptor(distance_function=distance, eps=lambda t: 0.1,
                    x=x, x_0=y, t=0, par=None)
 
     assert isinstance(ret, AcceptorResult)
@@ -28,6 +29,11 @@ def test_simple_acceptor():
     def model(par):
         return {'s0': par['p0'] + 1, 's1': 42}
 
-    prior = Distribution(p0=RV('uniform', -5, 10))
-    abc = ABCSMC(model, prior, NoDistance())
-    abc.new(
+    prior = pyabc.Distribution(p0=pyabc.RV('uniform', -5, 10))
+    abc = pyabc.ABCSMC(
+        model, prior, distance, population_size=2)
+    abc.new(pyabc.create_sqlite_db_id(), model({'p0': 1}))
+    h = abc.run(max_nr_populations=2)
+
+    df = h.get_weighted_distances()
+    assert np.isfinite(df['distance']).all()
