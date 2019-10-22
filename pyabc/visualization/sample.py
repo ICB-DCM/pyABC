ab3092500463ae2d87c7d0c10bcabd1069508c2a
Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>
2019-10-22 13:30:52 +0200

    Feature plot acceptance rate (#193)
    * add template for acc_rate und sample_nr trajectory plots
    
    * implement sample trajectory
    
    * finish samples trajectory
    
    * add test
    
    * fix unused variable
    
    * add acceptance rate plot; update adaptive distances notebook
    
    * rerun nb
    
    * lable should not be "Sam" ;) but "Samples"
    
    * fix codacy: remove unused variable
    
    * fix plot label defaults

--- pyabc/visualization/sample.py
+++ pyabc/visualization/sample.py
@@ -169,3 +169,177 @@ def plot_total_sample_numbers(
     fig.tight_layout()
 
     return ax
+
+
+def plot_sample_numbers_trajectory(
+        histories: Union[List, History],
+        labels: Union[List, str] = None,
+        rotation: int = 0,
+        title: str = "Required samples",
+        yscale: str = 'lin',
+        size: tuple = None,
+        ax: mpl.axes.Axes = None):
+    """
+    Plot of required sample number over all iterations, i.e. one trajectory
+    per history.
+
+    Parameters
+    ----------
+
+    histories: Union[List, History]
+        The histories to plot from. History ids must be set correctly.
+    labels: Union[List ,str], optional
+        Labels corresponding to the histories. If None are provided,
+        indices are used as labels.
+    rotation: int, optional (default = 0)
+        Rotation to apply to the plot's x tick labels. For longer labels,
+        a tilting of 45 or even 90 can be preferable.
+    title: str, optional (default = "Required samples")
+        Title for the plot.
+    yscale: str, optional (default = 'lin')
+        The scale on which to plot the counts. Can be one of 'lin', 'log'
+        (basis e) or 'log10'
+    size: tuple of float, optional
+        The size of the plot in inches.
+    ax: matplotlib.axes.Axes, optional
+        The axis object to use.
+
+    Returns
+    -------
+
+    ax: Axis of the generated plot.
+    """
+    # preprocess input
+    histories, labels = to_lists_or_default(histories, labels)
+
+    # create figure
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.get_figure()
+
+    # extract sample numbers
+    times = []
+    samples = []
+    for history in histories:
+        # note: the first entry corresponds to the calibration and should
+        # be included here to be fair against methods not requiring
+        # calibration
+        h_info = history.get_all_populations()
+        times.append(np.array(h_info['t']))
+        samples.append(np.array(h_info['samples']))
+
+    # apply scale
+    ylabel = "Samples"
+    if yscale == 'log':
+        samples = [np.log(sample) for sample in samples]
+        ylabel = "log(" + ylabel + ")"
+    elif yscale == 'log10':
+        samples = [np.log10(sample) for sample in samples]
+        ylabel = "log10(" + ylabel + ")"
+
+    # plot
+    for t, sample, label in zip(times, samples, labels):
+        ax.plot(t, sample, 'x-', label=label)
+
+    # add labels
+    ax.legend()
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel("Population index $t$")
+    # set size
+    if size is not None:
+        fig.set_size_inches(size)
+    fig.tight_layout()
+
+    return ax
+
+
+def plot_acceptance_rates_trajectory(
+        histories: Union[List, History],
+        labels: Union[List, str] = None,
+        rotation: int = 0,
+        title: str = "Acceptance rates",
+        yscale: str = 'lin',
+        size: tuple = None,
+        ax: mpl.axes.Axes = None):
+    """
+    Plot of acceptance rates over all iterations, i.e. one trajectory
+    per history.
+
+    Parameters
+    ----------
+
+    histories: Union[List, History]
+        The histories to plot from. History ids must be set correctly.
+    labels: Union[List ,str], optional
+        Labels corresponding to the histories. If None are provided,
+        indices are used as labels.
+    rotation: int, optional (default = 0)
+        Rotation to apply to the plot's x tick labels. For longer labels,
+        a tilting of 45 or even 90 can be preferable.
+    title: str, optional (default = "Acceptance rates")
+        Title for the plot.
+    yscale: str, optional (default = 'lin')
+        The scale on which to plot the counts. Can be one of 'lin', 'log'
+        (basis e) or 'log10'
+    size: tuple of float, optional
+        The size of the plot in inches.
+    ax: matplotlib.axes.Axes, optional
+        The axis object to use.
+
+    Returns
+    -------
+
+    ax: Axis of the generated plot.
+    """
+    # preprocess input
+    histories, labels = to_lists_or_default(histories, labels)
+
+    # create figure
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.get_figure()
+
+    # extract sample numbers
+    times = []
+    samples = []
+    pop_sizes = []
+    for history in histories:
+        # note: the first entry of time -1 is trivial and is thus ignored here
+        h_info = history.get_all_populations()
+        times.append(np.array(h_info['t'])[1:])
+        samples.append(np.array(h_info['samples'])[1:])
+        pop_sizes.append(np.array(
+            history.get_nr_particles_per_population().values[1:]))
+
+    # compute acceptance rates
+    rates = []
+    for sample, pop_size in zip(samples, pop_sizes):
+        rates.append(pop_size / sample)
+
+    # apply scale
+    ylabel = "Acceptance rate"
+    if yscale == 'log':
+        rates = [np.log(rate) for rate in rates]
+        ylabel = "log(" + ylabel + ")"
+    elif yscale == 'log10':
+        rates = [np.log10(rate) for rate in rates]
+        ylabel = "log10(" + ylabel + ")"
+
+    # plot
+    for t, rate, label in zip(times, rates, labels):
+        ax.plot(t, rate, 'x-', label=label)
+
+    # add labels
+    ax.legend()
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel("Population index $t$")
+    # set size
+    if size is not None:
+        fig.set_size_inches(size)
+    fig.tight_layout()
+
+    return ax
