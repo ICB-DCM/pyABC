4e62f6a41f914cdcffc837266c5b1ab22ed648d5
yannikschaelte <yannik.schaelte@gmail.com>
2019-11-04 17:04:16 +0100

    increase coverage of test_population

--- test/test_population.py
+++ test/test_population.py
@@ -18,10 +18,13 @@ def test_basic():
     assert len(pop.get_list()) == len(pop)
 
     weighted_distances = pop.get_weighted_distances()
+    weights, sumstats = pop.get_weighted_sum_stats()
     vals = pop.get_for_keys(
         keys=['weight', 'distance', 'parameter', 'sum_stat'])
     assert all(weighted_distances['w'] == vals['weight'])
     assert all(weighted_distances['distance'] == vals['distance'])
+    assert sumstats == vals['sum_stat']
+    assert all(weights == weighted_distances['w'])
 
     with pytest.raises(ValueError):
         pop.get_for_keys(['distance', 'w'])
