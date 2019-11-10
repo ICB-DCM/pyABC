7b835fd79a49c2b545bdb6c6d294640a3239b39c
yannikschaelte <yannik.schaelte@gmail.com>
2019-11-10 02:55:06 +0100

    adapt eps tests

--- test/test_epsilon.py
+++ test/test_epsilon.py
@@ -33,16 +33,16 @@ def test_quantileepsilon():
         quantile_multiplier=mpl, weighted=False)
 
     # check if initial value is respected
-    eps.initialize(0, lambda: df, None, None)
+    eps.initialize(0, lambda: df, lambda: None, None, None)
     assert np.isclose(eps(0), 5.1)
 
     # check if quantile is computed correctly
-    eps.update(1, df, None, None)
+    eps.update(1, lambda: df, lambda: None, None, None)
     assert np.isclose(eps(1), mpl * 2.5)
 
     # use other quantile
     eps = pyabc.QuantileEpsilon(alpha=0.9, weighted=True)
-    eps.initialize(0, lambda: df, None, None)
+    eps.initialize(0, lambda: df, lambda: None, None, None)
     assert eps(0) >= 3 and eps(0) <= 4
 
 
@@ -53,24 +53,33 @@ def test_medianepsilon():
 
 
 def test_temperature():
-    df = pd.DataFrame({
+    weighted_distances = pd.DataFrame({
         'distance': [1, 2, 3, 4],
         'w': [2, 1, 1, 0]})
+    all_records = [
+        dict(distance=np.random.randn(),
+             transition_pd_prev=np.random.randn(),
+             transition_pd=np.random.randn(),
+             accepted=True if np.random.random() > 0.5 else False)
+        for _ in range(20)]
     acceptor_config = {
         'pdf_norm': 5,
         'kernel_scale': pyabc.distance.SCALE_LOG}
     nr_pop = 3
     eps = pyabc.Temperature(initial_temperature=42)
-    eps.initialize(0, lambda: df, nr_pop, acceptor_config)
+    eps.initialize(0, lambda: weighted_distances, lambda: all_records,
+                   nr_pop, acceptor_config)
 
     # check if initial value is respected
-    assert np.isclose(eps(0), 42)
+    assert eps(0) == 42
 
-    eps.update(1, df, 0.4, acceptor_config)
+    eps.update(1, lambda: weighted_distances, lambda: all_records,
+               0.4, acceptor_config)
     assert eps(1) < 42
 
     # last time
-    eps.update(2, df, 0.2, acceptor_config)
+    eps.update(2, lambda: weighted_distances, lambda: all_records,
+               0.2, acceptor_config)
     assert eps(2) == 1
 
 
@@ -79,6 +88,12 @@ scheme_args = dict(
         'distance': [4, 5, 6, 1],
         'w': [0.25, 0.5, 0.10, 0.15]
     }),
+    get_all_records=lambda: [
+        dict(distance=np.random.randn(),
+             transition_pd_prev=np.random.randn(),
+             transition_pd=np.random.randn(),
+             accepted=True if np.random.random() > 0.5 else False)
+        for _ in range(20)],
     max_nr_populations=3,
     pdf_norm=10,
     kernel_scale=pyabc.distance.SCALE_LOG,
@@ -109,7 +124,8 @@ def test_scheme_acceptancerate():
     # high acceptance probabilities
     _scheme_args = copy.deepcopy(scheme_args)
     # change normalization s.t. most have 1.0 acceptance rate
-    _scheme_args['pdf_norm'] = 2
+    records = pd.DataFrame(_scheme_args['get_all_records']())
+    _scheme_args['pdf_norm'] = min(records['distance'])
     temp = scheme(t=0, **_scheme_args)
     assert temp == 1.0
 
