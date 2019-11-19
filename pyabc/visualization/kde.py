e13d6c665468fb7240f5a25ee505e81f37dc7ac1
yannikschaelte <yannik.schaelte@gmail.com>
2019-11-19 11:23:26 +0100

    remove code duplication in format_plot_matrix

--- pyabc/visualization/kde.py
+++ pyabc/visualization/kde.py
@@ -13,6 +13,7 @@ import pandas as pd
 
 from ..transition import MultivariateNormalTransition
 from ..storage import History
+from .util import format_plot_matrix
 
 
 def kde_1d(df, w, x, xmin=None, xmax=None, numx=50, kde=None):
@@ -489,44 +490,9 @@ def plot_kde_matrix(df, w,
             scatter(y, x, ax)
 
     # format
-    _format_kde_matrix(arr_ax, par_names)
+    format_plot_matrix(arr_ax, par_names)
 
     # adjust subplots to fit
     fig.tight_layout()
 
     return arr_ax
-
-
-def _format_kde_matrix(arr_ax, par_names):
-    """
-    Clear all labels and legends, and set the left-most and bottom-most
-    labels to the parameter names.
-
-    Parameters
-    ----------
-
-    arr_ax: array of matplotlib.axes.Axes
-        Shape (n_par, n_par) where len(par_names) == n_par.
-    par_names: list of str
-        Parameter names to be used as labels.
-    """
-    n_par = len(par_names)
-
-    for i in range(0, n_par):
-        for j in range(0, n_par):
-            # clear labels
-            arr_ax[i, j].set_xlabel("")
-            arr_ax[i, j].set_ylabel("")
-
-            # clear legends
-            arr_ax[i, j].legend = None
-
-            # remove spines
-            arr_ax[i, j].spines['right'].set_visible(False)
-            arr_ax[i, j].spines['top'].set_visible(False)
-
-    # set left-most and bottom-most labels to parameter names
-    for ax, label in zip(arr_ax[-1, :], par_names):
-        ax.set_xlabel(label)
-    for ax, label in zip(arr_ax[:, 0], par_names):
-        ax.set_ylabel(label)
--- pyabc/visualization/util.py
+++ pyabc/visualization/util.py
@@ -18,3 +18,38 @@ def to_lists_or_default(histories, labels=None):
         raise ValueError("The lengths of histories and labels do not match.")
 
     return histories, labels
+
+
+def format_plot_matrix(arr_ax, par_names):
+    """
+    Clear all labels and legends, and set the left-most and bottom-most
+    labels to the parameter names.
+
+    Parameters
+    ----------
+
+    arr_ax: array of matplotlib.axes.Axes
+        Shape (n_par, n_par) where len(par_names) == n_par.
+    par_names: list of str
+        Parameter names to be used as labels.
+    """
+    n_par = len(par_names)
+
+    for i in range(0, n_par):
+        for j in range(0, n_par):
+            # clear labels
+            arr_ax[i, j].set_xlabel("")
+            arr_ax[i, j].set_ylabel("")
+
+            # clear legends
+            arr_ax[i, j].legend = None
+
+            # remove spines
+            arr_ax[i, j].spines['right'].set_visible(False)
+            arr_ax[i, j].spines['top'].set_visible(False)
+
+    # set left-most and bottom-most labels to parameter names
+    for ax, label in zip(arr_ax[-1, :], par_names):
+        ax.set_xlabel(label)
+    for ax, label in zip(arr_ax[:, 0], par_names):
+        ax.set_ylabel(label)
