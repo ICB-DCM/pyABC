12b2095b2003238334db63865a7665735bf575e6
yannikschaelte <yannik.schaelte@gmail.com>
2019-11-19 10:26:55 +0100

    fix test_epsilon

--- test/test_epsilon.py
+++ test/test_epsilon.py
@@ -53,47 +53,43 @@ def test_medianepsilon():
 
 
 def test_temperature():
-    weighted_distances = pd.DataFrame({
-        'distance': [1, 2, 3, 4],
-        'w': [2, 1, 1, 0]})
-    all_records = [
-        dict(distance=np.random.randn(),
-             transition_pd_prev=np.random.randn(),
-             transition_pd=np.random.randn(),
-             accepted=True if np.random.random() > 0.5 else False)
-        for _ in range(20)]
+    weighted_distances = get_weighted_distances()
+    all_records = get_all_records(),
     acceptor_config = {
         'pdf_norm': 5,
         'kernel_scale': pyabc.distance.SCALE_LOG}
     nr_pop = 3
     eps = pyabc.Temperature(initial_temperature=42)
-    eps.initialize(0, lambda: weighted_distances, lambda: all_records,
+    eps.initialize(0, get_weighted_distances, get_all_records,
                    nr_pop, acceptor_config)
 
     # check if initial value is respected
     assert eps(0) == 42
 
-    eps.update(1, lambda: weighted_distances, lambda: all_records,
+    eps.update(1, get_weighted_distances, get_all_records,
                0.4, acceptor_config)
     assert eps(1) < 42
 
     # last time
-    eps.update(2, lambda: weighted_distances, lambda: all_records,
+    eps.update(2, get_weighted_distances, get_all_records,
                0.2, acceptor_config)
     assert eps(2) == 1
 
 
+get_weighted_distances = lambda: pd.DataFrame({
+        'distance': [1, 2, 3, 4],
+        'w': [2, 1, 1, 0]})
+
+get_all_records=lambda: [
+    dict(distance=np.random.randn(),
+         transition_pd_prev=np.random.randn(),
+         transition_pd=np.random.randn(),
+         accepted=True if np.random.random() > 0.5 else False)
+    for _ in range(20)]
+
 scheme_args = dict(
-    get_weighted_distances=lambda: pd.DataFrame({
-        'distance': [4, 5, 6, 1],
-        'w': [0.25, 0.5, 0.10, 0.15]
-    }),
-    get_all_records=lambda: [
-        dict(distance=np.random.randn(),
-             transition_pd_prev=np.random.randn(),
-             transition_pd=np.random.randn(),
-             accepted=True if np.random.random() > 0.5 else False)
-        for _ in range(20)],
+    get_weighted_distances=get_weighted_distances,
+    get_all_records=get_all_records,
     max_nr_populations=3,
     pdf_norm=10,
     kernel_scale=pyabc.distance.SCALE_LOG,
