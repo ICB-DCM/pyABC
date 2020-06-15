06d476f1e1af58e37c7334c14d981d111daa20db
yannikschaelte <yannik.schaelte@gmail.com>
2020-06-15 21:31:14 +0200

    remove macos -- not worth it

--- .github/workflows/ci_dependencies.sh
+++ .github/workflows/ci_dependencies.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-if [ "$(uname)" == "Darwin" ]; then
-  # MacOS
-  brew install redis
-else
-  # Linux
-  sudo apt-get update
-  sudo apt-get install redis-server
-fi
+sudo apt-get update
+# redis
+sudo apt-get install redis-server
 
 # pip
 python -m pip install --upgrade pip
@@ -23,24 +18,18 @@ do
 
     R)
 	    # R environment
-	    if [ "$(uname)" == "Darwin" ]; then
-        # MacOS
-        brew install r
-      else
-        # Linux
-        sudo apt-key adv \
-          --keyserver keyserver.ubuntu.com \
-          --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-        sudo add-apt-repository \
-          'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
-        sudo apt-get update
-        sudo apt-get install r-base
-      fi
+      sudo apt-key adv \
+        --keyserver keyserver.ubuntu.com \
+        --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+      sudo add-apt-repository \
+        'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+      sudo apt-get update
+      sudo apt-get install r-base
       pip install rpy2>=3.2.0 cffi>=1.13.1
     ;;
 
     petab)
-	    # PEtab
+	    # PEtab dependencies
       sudo apt-get install swig3.0 libatlas-base-dev libhdf5-serial-dev
       sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
       pip install https://github.com/icb-dcm/petab/archive/develop.zip
