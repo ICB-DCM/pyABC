31eb4449b9a857ecb03d1677c0df60cd460a170a
yannikschaelte <yannik.schaelte@gmail.com>
2020-08-01 19:45:57 +0200

    tset

--- .github/workflows/ci_dependencies.sh
+++ .github/workflows/ci_dependencies.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-if [ "$(uname)" == "Darwin" ]; then
-  # MacOS
-  brew install redis
-else
-  # Linux
-  sudo apt-get update
-  sudo apt-get install redis-server
-fi
-
 # pip
 python -m pip install --upgrade pip
-pip install -r .ci_pip_reqs.txt
-
-# install package
-pip install -e .
 
 # optional dependencies
 for par in "$@"
 do
   case $par in
+    base)
+      # basic setup
+      if [ "$(uname)" == "Darwin" ]; then
+        # MacOS
+        brew install redis
+      else
+        # Linux
+        sudo apt-get update
+        sudo apt-get install redis-server
+      fi
+    ;;
 
     R)
-	    # R environment
+      # R environment
       if [ "$(uname)" == "Darwin" ]; then
         # MacOS
         brew install r
@@ -36,16 +34,12 @@ do
         sudo apt-get update
         sudo apt-get install r-base
       fi
-      pip install rpy2>=3.2.0 cffi>=1.13.1
     ;;
 
     petab)
-	    # PEtab
+      # PEtab
       sudo apt-get install swig3.0 libatlas-base-dev libhdf5-serial-dev
       sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
-      pip install https://github.com/icb-dcm/petab/archive/develop.zip
-      pip install -e \
-        'git+https://github.com/icb-dcm/amici.git@develop#egg=amici&subdirectory=python/sdist'
       git clone --depth 1 \
         https://github.com/petab-dev/petab_test_suite .tmp/petab_test_suite
       pip install -e .tmp/petab_test_suite
@@ -54,6 +48,11 @@ do
     *)
       echo "Unknown argument" >&2
 	  exit 1
+    ;;
+
+    docs)
+      # documentation
+      sudo apt-get install pandoc
     ;;
   esac
 done
