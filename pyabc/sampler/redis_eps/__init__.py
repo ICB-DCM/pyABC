6196f0def39614113124251d6723993b9912b213
yannikschaelte <yannik.schaelte@gmail.com>
2020-12-09 23:11:48 +0100

    fixup

--- pyabc/sampler/redis_eps/__init__.py
+++ pyabc/sampler/redis_eps/__init__.py
@@ -1,3 +1,5 @@
+"""Samplers using a redis server as communication platform."""
+
 from .sampler import RedisEvalParallelSampler
 from .sampler_static import RedisStaticSampler
 from .redis_sampler_server_starter import (
