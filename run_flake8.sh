3930b8e470645d27f4479dec5c8afad1c2b718c9
yannikschaelte <yannik.schaelte@gmail.com>
2020-12-31 15:10:14 +0100

    fix flake8

--- run_flake8.sh
+++ run_flake8.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+PER_FILE_IGNORES='
+*/__init__.py:F401
+test/*.py:T001,S101
+test_performance/*.py:T001
+*/cli.py:T001'
+
 python3 -m flake8 pyabc test test_performance \
   --extend-ignore='S403,S301,C408' \
-  --per-file-ignores='*/__init__.py:F401 test/*:T001,S101 */cli.py:T001'
+  --per-file-ignores="$PER_FILE_IGNORES"
