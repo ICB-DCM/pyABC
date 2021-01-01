2ab332066e6b0cfc8556eaea4c39e7dbecca1772
yannikschaelte <yannik.schaelte@gmail.com>
2021-01-01 15:46:48 +0100

    test restart for redis look-ahead

--- test/base/test_resume_run.py
+++ test/base/test_resume_run.py
@@ -1,6 +1,9 @@
-from pyabc import (ABCSMC, Distribution, RV)
 import pytest
 import numpy as np
+
+from pyabc import (
+    ABCSMC, Distribution, RV, RedisEvalParallelSamplerServerStarter,
+    MulticoreEvalParallelSampler)
 
 
 @pytest.fixture(params=[0, None])
@@ -8,7 +11,28 @@ def gt_model(request):
     return request.param
 
 
-def test_resume(db_path, gt_model):
+def RedisEvalParallelSamplerLookAheadDelayWrapper(**kwargs):
+    return RedisEvalParallelSamplerServerStarter(
+        look_ahead=True, look_ahead_delay_evaluation=True, **kwargs)
+
+
+@pytest.fixture(params=[
+    MulticoreEvalParallelSampler,
+    RedisEvalParallelSamplerLookAheadDelayWrapper,
+])
+def sampler(request):
+    s = request.param()
+    try:
+        yield s
+    finally:
+        # release all resources
+        try:
+            s.shutdown()
+        except AttributeError:
+            pass
+
+
+def test_resume(db_path, gt_model, sampler):
     def model(parameter):
         return {"data": parameter["mean"] + np.random.randn()}
 
@@ -19,14 +43,14 @@ def test_resume(db_path, gt_model):
         y_data = y["data"]
         return abs(x_data - y_data)
 
-    abc = ABCSMC(model, prior, distance, population_size=10)
+    abc = ABCSMC(model, prior, distance, population_size=10, sampler=sampler)
     history = abc.new(db_path, {"data": 2.5}, gt_model=gt_model)
     run_id = history.id
     print("Run ID:", run_id)
     hist_new = abc.run(minimum_epsilon=0, max_nr_populations=1)
     assert hist_new.n_populations == 1
 
-    abc_continued = ABCSMC(model, prior, distance)
+    abc_continued = ABCSMC(model, prior, distance, sampler=sampler)
     run_id_continued = abc_continued.load(db_path, run_id)
     print("Run ID continued:", run_id_continued)
     hist_contd = abc_continued.run(minimum_epsilon=0, max_nr_populations=1)
