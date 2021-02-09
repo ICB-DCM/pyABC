4eca8e88dbfd07d731e8604caf0b3bafa8e8e1fc
yannikschaelte <yannik.schaelte@gmail.com>
2021-02-09 23:28:50 +0100

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-from setuptools import setup
-import os
+"""Set up module."""
+import setuptools
 
-# extract version
-with open(os.path.join(os.path.dirname(__file__), "pyabc", "version.py")) as f:
-    version = f.read().split("\n")[0].split("=")[-1].strip(' ').strip('"')
-
-# all other information comes from setup.cfg
-
-setup(version=version)
+setuptools.setup()
