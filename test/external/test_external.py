0b8beda3b3b5fc714760416b608757132376a7bc
Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>
2021-11-23 17:27:44 +0100

    Efficiently interface Julia simulators via pyjulia (#514)
    * add julia interface
    
    * enable julia module pickling
    
    * add julia tests
    
    * install pyjulia
    
    * tidy up demo example locs
    
    * remove pickling req
    
    * docs
    
    * fix matplotlib int error
    
    * docs
    
    * docs
    
    * stuff
    
    * whatever
    
    * link docs
    
    * fixix
    
    * fix typo
    
    * test ipython write

--- test/external/test_external.py
+++ test/external/test_external.py
@@ -1,8 +1,5 @@
 import os
-import tempfile
 
-import numpy as np
-import pandas as pd
 import pytest
 
 import pyabc
@@ -37,63 +34,8 @@ def sampler(request):
             pass
 
 
-def test_rpy2(sampler):
-    # run the notebook example
-    r_file = "doc/examples/myRModel.R"
-    r = pyabc.external.R(r_file)
-    r.display_source_ipython()
-    model = r.model("myModel")
-    distance = r.distance("myDistance")
-    sum_stat = r.summary_statistics("mySummaryStatistics")
-    data = r.observation("mySumStatData")
-    prior = pyabc.Distribution(
-        meanX=pyabc.RV("uniform", 0, 10), meanY=pyabc.RV("uniform", 0, 10)
-    )
-    abc = pyabc.ABCSMC(
-        model,
-        prior,
-        distance,
-        summary_statistics=sum_stat,
-        sampler=sampler,
-        population_size=5,
-    )
-    db = pyabc.create_sqlite_db_id(file_="test_external.db")
-    abc.new(db, data)
-    history = abc.run(minimum_epsilon=0.9, max_nr_populations=2)
-    history.get_weighted_sum_stats_for_model(m=0, t=1)[1][0]["cars"].head()
-
-    # try load
-    id_ = history.id
-    abc = pyabc.ABCSMC(
-        model,
-        prior,
-        distance,
-        summary_statistics=sum_stat,
-        sampler=sampler,
-        population_size=6,
-    )
-    # shan't even need to pass the observed data again
-    abc.load(db, id_)
-    abc.run(minimum_epsilon=0.1, max_nr_populations=2)
-
-
-def test_rpy2_details():
-    # check using a py model and an r sumstat
-    def model(pars):
-        df = pd.DataFrame(
-            {'s0': pars['p0'] + np.random.randn(10), 's1': np.random.randn(10)}
-        )
-        file_ = tempfile.mkstemp()[1]
-        df.to_csv(file_)
-        return {'loc': file_}
-
-    r = pyabc.external.R("doc/examples/rmodel/sumstat_py.r")
-    sumstat = r.summary_statistics("sumstat", is_py_model=True)
-    sumstat(model({'p0': 42}))
-
-
 def test_external():
-    folder = "doc/examples/rmodel/"
+    folder = "doc/examples/model_r/"
     executable = "Rscript"
 
     # initialize
