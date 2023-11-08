558d5e4e7673e2aa749596cb2861a24c1cc2dab8
Stephan Grein <stephan.grein@uni-bonn.de>
2023-11-08 15:50:17 +0100

    Fix assets for pyabc visserver. (#616)
    * Fix assets for pyabc visserver.
    
    * Update R.
    
    * Fix for logo path in abc-server-dash
    
    * Preparing release notes for next patch release.
    
    * Updating about.rst.
    
    * Updating changelog.

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -34,11 +34,8 @@ do
         brew install r
       else
         # Linux
-        sudo apt-key adv \
-          --keyserver keyserver.ubuntu.com \
-          --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-        sudo add-apt-repository \
-          'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+        wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/r-project.gpg
+        echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
         sudo apt-get update
         sudo apt-get install r-base
       fi
