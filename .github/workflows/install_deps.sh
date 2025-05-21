4200ed6c535bf9b5ea4b856814d430911cb09721
Stephan Grein <stephan.grein@uni-bonn.de>
2025-05-21 17:21:07 +0200

    Try to fix R dependencies.

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -37,7 +37,7 @@ do
         wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/r-project.gpg
         echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
         sudo apt-get update
-        sudo apt-get install r-base
+        sudo apt-get install libtiff5 r-base
       fi
     ;;
 
