12609690cfde313a8261cdb7985cdc0e99a45563
Stephan Grein <stephan.grein@uni-bonn.de>
2025-06-03 16:11:44 +0200

    Creating README.rst automatically.

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 import setuptools
 
-setuptools.setup()
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
+setuptools.setup(
+    long_description = long_description,
+    long_description_content_type="text/x-rst"
+)
