fcb137a18da875b9048b2c786acaf75666b09156
arrjon <jonas.arruda@uni-bonn.de>
2026-02-18 11:55:14 +0100

    fix dask for python3.12

--- pyabc/sampler/dask_sampler.py
+++ pyabc/sampler/dask_sampler.py
@@ -43,7 +43,7 @@ class DaskDistributedSampler(EPSMixin, Sampler):
         self,
         dask_client: Client = None,
         client_max_jobs: int = np.inf,
-        default_pickle: bool = True,
+        default_pickle: bool = False,
         batch_size: int = 1,
     ):
         # Assign Client
