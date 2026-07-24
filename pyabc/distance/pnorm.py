Update particle weight normalization to global convention and fix minor bugs.

pyabc/distance/pnorm.py
@@ -146,7 +146,7 @@ class PNormDistance(Distance):
 
     @staticmethod
     def format_dict(
-        vals: dict[str, float] | dict[int, dict[str, float]],
+        vals: dict[str, float] | dict[int, dict[str, float]] | None,
         t: int,
         s_ids: list[str],
     ) -> dict[int, float | np.ndarray]:
@@ -170,8 +170,7 @@ class PNormDistance(Distance):
             vals = {t: vals}
 
         # convert dicts to arrays
-        for _t, dct in vals.items():
-            vals[_t] = dict2arr(dct, keys=s_ids)
+        vals = {_t: dict2arr(dct, keys=s_ids) for _t, dct in vals.items()}
 
         return vals
 
@@ -320,7 +319,9 @@ class AdaptivePNormDistance(PNormDistance):
         # call p-norm constructor
         super().__init__(p=p, fixed_weights=fixed_weights, sumstat=sumstat)
 
-        self.initial_scale_weights: dict[str, float] = initial_scale_weights
+        self.initial_scale_weights: dict[str, float] | None = (
+            initial_scale_weights
+        )
 
         self.scale_weights: dict[int, np.ndarray] = {}
 

pyabc/storage/json.py
@@ -19,7 +19,7 @@ def save_dict_to_json(dct: dict, file_: str):
     for key, val in dct.items():
         # cannot handle ndarrays
         if isinstance(val, np.ndarray):
-            dct[key] = list(val)
+            dct[key] = val.tolist()
     with open(file_, 'w') as f:
         json.dump(dct, f)
 

pyabc/util/dict2arr.py
@@ -41,8 +41,7 @@ def dict2arr(dct: dict | np.ndarray, keys: list) -> np.ndarray:
     if len(arr) == 1:
         return np.asarray(arr[0])
     # flatten
-    arr = [val for sub_arr in arr for val in sub_arr]
-    return np.asarray(arr)
+    return np.concatenate([np.asarray(sub_arr) for sub_arr in arr])
 
 
 def dict2arrlabels(dct: dict, keys: list) -> list[str]: